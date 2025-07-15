101e8158c50f3933e764d489c1fcadd937f35900
TobiasM <ingcpu@gmail.com>
2025-07-15 13:49:59 -0500

    Update index.js
    user

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ app.post("/correo", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
+
 const eliminarUsuario = require("./firebaseDelete"); // importa el script
 
 app.post("/eliminar-usuario", async (req, res) => {
